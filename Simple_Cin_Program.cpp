f429cea34820e5632e15432d8d1c9710406a18be
Brij Shah <56499450+bshah016@users.noreply.github.com>
2020-01-07 20:39:28 -0800

    Add files via upload

---
+++ Simple_Cin_Program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string userName;
+   
+   cin >> userName;
+   cout << "Hello " << userName << ", and welcome to CS Online!" << endl;
+
+   return 0;
+}
